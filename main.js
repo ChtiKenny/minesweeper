da774de3edd8c3acddd7a410c43d743ba4a3a0e1
ChtiKenny <laurent.corentin.k@gmail.com>
2018-11-12 11:34:49 +0100

    created grid and cell class

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+class Cell {
+  constructor(a, b, isTrap) {
+    this.x = a;
+    this.y = b;
+    this.bomb = isTrap ? true : false;
+    this.revealed = false;
+  }
+  reveal(){
+    this.revealed = true;
+  }
+}
+
+class Grid {
+  constructor(rows, cols, bombs, size) {
+    this.cells = new Array(rows).fill(new Array(cols));
+  }
+}
